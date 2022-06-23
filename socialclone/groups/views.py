19bcce2504129eb0d91494deec26e509e4376039
manavshah <manavshah1011.ms@gmail.com>
2022-06-23 21:33:32 +0530

    join and leavve group views and urls in groups

--- socialclone/groups/views.py
+++ socialclone/groups/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.db import IntegrityError
+from django.shortcuts import get_object_or_404, render
 from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
-
+from django.contrib import messages
 from django.urls import reverse
 from django.views import generic
 from .models import GroupMember,Group
@@ -15,3 +16,36 @@ class SingleGroup(LoginRequiredMixin,generic.DetailView):
     
 class ListGroups(LoginRequiredMixin,generic.ListView):
     model=Group
+
+class JoinGroup(LoginRequiredMixin,generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single',kwargs={'slug':self.kwargs.get('slug')})
+    
+    def get(self,request, *args, **kwargs):
+        group=get_object_or_404(Group,slug=self.kwargs.get('slug'))
+        
+        try:
+            GroupMember.objects.create(user=self.request.user,group=group)
+        
+        except IntegrityError:
+            messages.warning(request,'Warning already a member!')
+        else:
+            messages.success(request,'You are now a member')
+        
+        return super().get(request,*args,**kwargs)
+    
+class LeaveGroup(LoginRequiredMixin,generic.RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('groups:single',kwargs={'slug':self.kwargs.get('slug')})
+        
+    def get(self,request,*args, **kwargs):
+        try:
+            membership=GroupMember.objects.filter(
+                user=request.user,
+                group__slug=self.kwargs.get('slug')
+            ).get()
+        except GroupMember.DoesNotExist:
+            messages.warning(request,'Sorry you are not in this group!')
+        else:
+            messages.success(request,'You have left this gouop')
+        return super().get(request,*args,**kwargs)
