d0358a7622ea6dc75eb79511d26338aec4af331d
manavshah <manavshah1011.ms@gmail.com>
2022-06-23 19:26:50 +0530

    models of groups and posts

--- socialclone/groups/models.py
+++ socialclone/groups/models.py
@@ -1,3 +1,39 @@
+from enum import unique
+from typing import OrderedDict
+from django.urls import reverse
 from django.db import models
+from django.utils.text import slugify
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User=get_user_model()
+
+class Group(models.Model):
+    name=models.CharField(max_length=255, unique=True)
+    slug=models.SlugField(allow_unicode=True,unique=True)
+    description=models.TextField(blank=True,default='')
+    member=models.ManyToManyField(User,through='GroupMember')
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self,*args, **kwargs):
+        super().save(*args, **kwargs)
+        self.slug=slugify(self.name)
+    
+    def get_absolute_url(self):
+        return reverse("groups:single", kwargs={"slug": self.slug})
+    
+    class Meta:
+        ordering=['name']
+    
+        
+class GroupMember(models.Model):
+    group=models.ForeignKey(Group,on_delete=models.CASCADE,related_name='groups')
+    user=models.ForeignKey(User, on_delete=models.CASCADE,related_name='user_groups')
+    
+    def __str__(self):
+        return self.user.username
+     
+    class Meta:
+        unique_together =('group''user')
--- socialclone/posts/models.py
+++ socialclone/posts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.urls import reverse
+from django.conf import settings
+from groups.models import Group
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User=get_user_model()
+
+class Post(models.Model):
+    user=models.ForeignKey(User,related_name='user_posts')
+    created_at=models.DateTimeField(auto_now=True)
+    message=models.TextField(blank=False,null=False)
+    group=models.ForeignKey(Group,related_name='group_posts',null=True,blank=True)
+    
+    def __str__(self):
+        return self.message
+
+    def get_absolute_url(self):
+        return reverse("post:single", kwargs={"username": self.user.username,'pk':self.pk})
+    
+    class Meta:
+        ordering=['created_at']
+        unique_together=['user','message']
+        
