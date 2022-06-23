58ebb70f0b396cb29409d4b57e473d3875e0ee73
manavshah <manavshah1011.ms@gmail.com>
2022-06-23 19:56:36 +0530

    templates for groups

--- socialclone/groups/views.py
+++ socialclone/groups/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
 
+from django.urls import reverse
+from django.views import generic
+from .models import GroupMember,Group
 # Create your views here.
+
+class CreateGroup(LoginRequiredMixin,generic.CreateView):
+    fields=('name','description')
+    model=Group
+    
+class SingleGroup(LoginRequiredMixin,generic.DetailView):
+    model=Group
+    
+class ListGroups(LoginRequiredMixin,generic.ListView):
+    model=Group
